50d82cc82faeb283cd4620410f1405b978e8c25f
MikDR2000 <75440924+MikDR2000@users.noreply.github.com>
2021-01-08 16:11:16 +0100

    Package MeteoDati

--- MeteoDati/Vento.java
+++ MeteoDati/Vento.java
@@ -7,28 +7,13 @@ package com.esame_prog_meteo.MeteoDati;
  */
 
 public class Vento extends DatiMeteo{
-	/**
-	 * Description of the property velocita.
-	 */
 	protected float velocita;
-
-	/**
-	 * Description of the property direzione.
-	 */
 	protected int direzione;
-
-	/**
-	 * Description of the property raffica.
-	 */
 	protected float raffica;
-	
-	/**
-	 * Description of the property isRaffica.
-	 */
 	protected boolean isRaffica;
 
 	/**
-	 * The constructor 1 there is raffica.
+	 * In questo costruttore è presente l'attributo raffica.
 	 */
 	public Vento (String nome, int idCitta, int cod, int timezone, long dR, int visibilita, String base, float velocita, int direzione, float raffica) {
 		super(nome, idCitta, cod, timezone, dR, visibilita, base);
@@ -38,44 +23,29 @@ public class Vento extends DatiMeteo{
 	}
 	
 	/**
-	 * The constructor 2 there isn't raffica.
+	 * In questo costruttore non è presente l'attributo raffica.
 	 */
 	public Vento(String nome, int idCitta, int cod, int timezone, long dR, int visibilita, String base, float velocita, int direzione) {
 		super(nome, idCitta, cod, timezone, dR, visibilita, base);
 		this.velocita = velocita;
 		this.direzione = direzione;
 	}
-	
-	/**
-	 * The constructor 3 isRaffica boolean.
-	 */
-	public Vento(String nome, int idCitta, int cod, int timezone, long dR, int visibilita, String base, 
-				 float velocita, int direzione, boolean isRaffica) {
-		super(nome, idCitta, cod, timezone, dR, visibilita, base);
-		this.velocita = velocita;
-		this.direzione = direzione;
-		this.isRaffica = isRaffica;
-	}
-	
 
 	/**
-	 * Returns velocita.
 	 * @return velocita 
 	 */
 	public float getVelocita() {
 		return this.velocita;
 	}
 
-	/**
-	 * Sets a value to attribute velocita. 
-	 * @param newVelocita 
+	/** 
+	 * @param newVelocita è la nuova velocità 
 	 */
 	public void setVelocita(float newVelocita) {
 		this.velocita = newVelocita;
 	}
 
 	/**
-	 * Returns direzione.
 	 * @return direzione 
 	 */
 	public int getDirezione() {
@@ -83,15 +53,13 @@ public class Vento extends DatiMeteo{
 	}
 
 	/**
-	 * Sets a value to attribute direzione. 
-	 * @param newDirezione 
+	 * @param newDirezione è la nuova direzione
 	 */
 	public void setDirezione(int newDirezione) {
 		this.direzione = newDirezione;
 	}
 
 	/**
-	 * Returns raffica.
 	 * @return raffica 
 	 */
 	public float getRaffica() {
@@ -99,32 +67,14 @@ public class Vento extends DatiMeteo{
 	}
 
 	/**
-	 * Sets a value to attribute raffica. 
-	 * @param newRaffica 
+	 * @param newRaffica è la nuova raffica.
 	 */
 	public void setRaffica(float newRaffica) {
 		this.raffica = newRaffica;
 	}
 	
 	/**
-	 * Returns isRraffica.
-	 * @return isRaffica 
-	 */
-	public boolean getisRaffica() {
-		return this.isRaffica;
-	}
-
-	/**
-	 * Sets a value to attribute isRaffica. 
-	 * @param newisRaffica 
-	 */
-	public void setRaffica(boolean newisRaffica) {
-		this.isRaffica = newisRaffica;
-	}
-	
-	/**
-	 * Description of the method toString.
-	 * @return 
+	 * @return dati del vento
 	 */
 	public String toString() {
 		return ("Velocita: " + velocita + "; Direzione: " + direzione + "; Raffica: " + raffica);
