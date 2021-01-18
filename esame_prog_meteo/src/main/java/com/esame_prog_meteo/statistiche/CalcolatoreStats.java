32e12589904fa9163cec24978233c63a3fc9f38e
MikDR2000 <75440924+MikDR2000@users.noreply.github.com>
2021-01-18 16:53:33 +0100

    Package delle Statistiche

--- esame_prog_meteo/src/main/java/com/esame_prog_meteo/statistiche/CalcolatoreStats.java
+++ esame_prog_meteo/src/main/java/com/esame_prog_meteo/statistiche/CalcolatoreStats.java
@@ -6,8 +6,8 @@ import com.esame_prog_meteo.model.Vento;
 
 /**
  * Interfaccia che definisce il metodo necessario per restituire il valore delle stastistiche
- * @author joshuaciccolini
- *
+ * @author Joshua Ciccolini
+ * @author Michele Di Renzo
  */
 public interface CalcolatoreStats {
 	public float getRisStats(Vector<Vento> v);
